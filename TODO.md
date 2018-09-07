4873979ec26f27ca1fba99befeea9c2ad206fd16
Heather Piverotto <heather@Huberts-MacBook-Pro.local>
2018-09-07 11:45:33 -0700

    adding delete function and happen to add basic edit at the same time

---
+++ TODO.md
@@ -0,0 +1,34 @@
+#MVP
+- [ ] store items
+  - [v] create new item
+  - [v] select item
+  - [v] edit item
+  - [v] delete item
+
+#Detail Todo
+- [ ] front end
+  - [v] display items 
+  - [v] enter new item
+  - [ ] select item for edit
+  - [ ] select item for delete
+  - [v] a form!
+
+- [ ] UI/UX
+  - [ ] confirmation of delete/update
+  - [ ] sortable list 
+  - [ ] navigation/pagination
+  - [ ] mouse over preview
+  - [ ] searching/filtering
+  - [ ] animations/transitions
+  
+- [ ] Library Considerations
+  - [ ] underscore
+  - [v] jquery
+  - [ ] moment.js
+  - [ ] c3.js (charts.js)
+  
+# Next Steps
+- [ ] Testing
+- [ ] Filter out common functionality
+
+
--- app.js
+++ app.js
@@ -1,23 +1,51 @@
 $(document).ready(function() {
 
   $(".add-text-btn").on("click", function() {
-  	let inputValue = $(".user-input").val();
-    localStorage.setItem("testStorage", inputValue);
+
+  	// store values
+  	let inputKey = $(".user-input-title").val();
+  	let inputValue = $(".user-input-body").val();
+
+  	//clear values
+  	$(".user-input-title").val("");
+    $(".user-input-body").val("");
+
+  	console.log(inputKey, inputValue);
+
+    localStorage.setItem(inputKey, inputValue);
+
     /*alert( "value from local storage" + localStorage.getItem("testStorage") );*/
-    
-    $(".display").text(localStorage.getItem("testStorage"));
+    let itemHtml = '<div class="display-item" data-storage-key="'+ inputKey +'"> ' + inputKey + ' ' + localStorage.getItem(inputKey) + '</div>';
+    $(".display").html(itemHtml);
+    //console.log(localStorage);
+    //how can we delegate this event to the outer html node?
+    //https://learn.jquery.com/events/event-delegation
+    $(".display-item").on("click", function(e) {
+      // plop the key:value back into the input boxes
+
+  	  // get the values from the divs?
+  	  console.log("key=> ", e.target.dataset.storageKey); // user-input-field
+  	  localStorage.getItem(e.target.dataset.storageKey); //user-input-body
+
+  	    // set those values in the form fields 
+  	    $("user-input-title").val(e.target.dataset.storageKey);
+  	    $("user-input-body").val(localStorage.getItem(e.target.dataset.storageKey));
+    });
+
   });
+
+  
 
   /*Allowing for previewing text live*/
+  // TODO add back in later
+  // $(".user-input").on("keyup", function() {
+  // 	let inputValue = $(".user-input").val();
+  //   localStorage.setItem("testStorage", inputValue);
+  //   $(".display").text(localStorage.getItem("testStorage"));
+  // });
 
-  $(".user-input").on("keyup", function() {
-  	let inputValue = $(".user-input").val();
-    localStorage.setItem("testStorage", inputValue);
-    $(".display").text(localStorage.getItem("testStorage"));
-  });
-
-  $(".delete-text.btn").on("click", function() {
-  	localStorage.removeItem("testStorage");
+  $(".del-text-btn").on("click", function() {
+  	localStorage.removeItem($('.user-input-title').val() );
   	alert('item deleted? check console');
   });
 
